144e81a748626d91f14eec4b9f80dbab830309b9
Arakimi <caio.hunter@hotmail.com>
2019-02-19 22:36:14 -0300

    implementação de modelo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Carro } from '../modelos/carro';
 
 @Component({
   selector: 'app-home',
@@ -7,21 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit{
   
-  public carros;
+  public carros: Carro[];
 
   ngOnInit(): void {
     this.carros = [
       {
-        "nome":"Fusca",
-        "preco":1000
+        nome:"Fusca",
+        preco:1000
       },
       {
-        "nome":"Gol",
-        "preco":2000
+        nome:"Gol",
+        preco:2000
       },
       {
-        "nome":"Astra",
-        "preco":3000
+        nome:"Astra",
+        preco:3000
       },
     ]
   }
