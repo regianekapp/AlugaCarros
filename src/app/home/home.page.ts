8c4e028aa53f04ab0a1c6544dc80170b6e526154
regiane <regianekapp@gmail.com>
2019-02-19 22:17:10 -0300

    implementacao de lusta estatica no arquivo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+   public carros;
+   /*
+   constructor(){
+    this.carro = [
+    {"nome":"Fusca"},
+    {"nome":"Gol"},
+    {"nome":"Astra"},
+    ]
+   }*/
+   ngOnInit():void {
+     this.carros = [
+      {
+        "nome":"Fusca",
+        "preco": 2000
+      },
+      {
+        "nome":"Gol",
+        "preco": 3000
+      },
+      {
+        "nome":"Astra",
+        "preco": 4000
+      },
+      ]
+   }
 }
