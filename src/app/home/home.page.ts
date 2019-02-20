bbe76e17da6785f7a73c41dfb85018daeed79399
Arakimi <caio.hunter@hotmail.com>
2019-02-19 22:16:07 -0300

    Implementação Lista estatica no arquivo .ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  
+  public carros;
 
+  ngOnInit(): void {
+    this.carros = [
+      {
+        "nome":"Fusca",
+        "preco":1000
+      },
+      {
+        "nome":"Gol",
+        "preco":2000
+      },
+      {
+        "nome":"Astra",
+        "preco":3000
+      },
+    ]
+  }
 }
