cbb25540e283e43680a2db65adf8299ce82f8c5d
Arakimi <caio.hunter@hotmail.com>
2019-03-11 22:21:42 -0300

    Implementação de loading e alert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Carro } from '../modelos/carro';
-import { HttpClient } from '@angular/common/http'; //Import HTTPCLIENT
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'; //Import HTTPCLIENT
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +12,38 @@ export class HomePage implements OnInit{
   
   public carros: Carro[];
 
-    constructor(public http:HttpClient){}
+    constructor(public http:HttpClient,
+      private loadingCtrl:LoadingController, //Importa a biblioteca loading
+      private alertCtrl:AlertController
+      ){}
 
-  ngOnInit(): void {
+  async ngOnInit(){ //async deixa a exibição assincrona
+    
+
+    const loading = await this.loadingCtrl.create({
+      message:'Aguarde enquantos os carros são carregados !!!'
+    });
+
+    await loading.present(); //Exibi na tela o loading
+
+    //Pode aparecer antes mesmo do loading aparecer
     this.http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
     .subscribe(
       (carros)=>{
         this.carros = carros;
+      },
+      async (err:HttpErrorResponse)=>{
+        console.log('Deu Erro '+ err.status);
+        const al = await this.alertCtrl.create({
+          header:'Erro',
+          message:'Erro ao carregar pagina',
+          buttons: [{text: 'OK'}]
+        });
+        await al.present();
+      }
+    ).add(
+      ()=>{
+        loading.dismiss(); //Desliga o loading da tela se já estiver carregado os arquivos
       }
     )
   }
